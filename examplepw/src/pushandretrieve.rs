771762ab4a1604bc8b301c62e890cf0452002999
allevs01 <stefanoallevi01@gmail.com>
2024-06-27 16:11:59 +0200

    added full example file

--- examplepw/src/pushandretrieve.rs
+++ examplepw/src/pushandretrieve.rs
@@ -17,6 +17,7 @@ use identity_iota::credential::DecodedJwtCredential;
 use identity_iota::credential::Jwt;
 use identity_iota::credential::JwtCredentialValidationOptions;
 use identity_iota::credential::JwtCredentialValidator;
+use identity_iota::credential::JwtCredentialValidatorUtils;
 use identity_iota::iota::IotaDID;
 use identity_iota::iota::IotaIdentityClientExt;
 use iota_sdk::client::Client;
@@ -25,6 +26,7 @@ use examplepw::utils::API_ENDPOINT;
 use examplepw::identitylib::{create_vc, get_issuer_storage};
 use identity_iota::credential::FailFast;
 use identity_iota::iota::IotaDocument;
+use iota_sdk::types::block::payload::Payload;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -34,10 +36,9 @@ async fn main() -> anyhow::Result<()> {
         .finish()
         .await?;
 
-    //change this did with another one you generate
+    //change these dids with other ones you generate with the first example
     let did_string_issuer = "did:iota:snd:0xdd69166eea24b073a7f6a849e9533fe16d48500edcd4b3f17f9cfef1249bd625";
     let did_string_holder = "did:iota:snd:0x8d5b36023bc20e9bc26bb3353a5d50bf31e29b75aff01fd962419ff69e77ad2c";
-
 
     let did_holder: IotaDID = IotaDID::parse(did_string_holder).unwrap();
     let did_issuer: IotaDID = IotaDID::parse(did_string_issuer).unwrap();
@@ -57,7 +58,6 @@ async fn main() -> anyhow::Result<()> {
 
     //issue vc
     let credential_jwt = create_vc_example(&storage,&fragment,&issuer_document,&holder_document).await?;
-    
     println!("credential jwt : {:#?}", credential_jwt);
     
     //verify credential
@@ -72,9 +72,61 @@ async fn main() -> anyhow::Result<()> {
             .unwrap();
 
     println!("VC successfully validated");
-
     println!("Credential JSON > {:#}", decoded_credential.credential);
 
+    //set the tag and the payload of the block
+    let tag = std::env::args().nth(1).unwrap_or_else(|| "My first tag".to_string());
+    let data = std::env::args().nth(2).unwrap_or_else(|| credential_jwt.as_str().to_string());
+
+    // Create and send the block with tag and data, this method returns the block pushed
+    let block = client
+        .build_block()
+        .with_tag(tag.as_bytes().to_vec())
+        .with_data(data.as_bytes().to_vec())
+        .finish()
+        .await?;
+
+    println!("{block:#?}\n");
+
+    println!(
+        "Block with tag and data sent: {}/block/{}",
+        "https://explorer.shimmer.network/testnet",
+        block.id()
+    );
+
+    //for testing purpose we will use the block id directly to get the block (even if we already have it)
+    
+    let block1 = client.get_block(&block.id()).await?;
+
+    let mut jwt_string:String = "".to_string();
+
+    //we recover the payload (credential jwt) from the block
+    if let Some(Payload::TaggedData(payload)) = block1.payload() {
+        jwt_string = String::from_utf8(payload.data().to_vec()).expect("found invalid UTF-8");
+    }
+
+    //the recovered jwt
+    let jwt = Jwt::from(jwt_string.to_string());
+
+    println!("{:#?}", jwt);
+
+    //Now, to be sure, we verify again the credential jwt by extracting the issuer from it
+    let issuer_did: IotaDID = JwtCredentialValidatorUtils::extract_issuer_from_jwt(&jwt)?;
+    let issuer1_document: IotaDocument = client.resolve_did(&issuer_did).await?;
+
+    let decoded_credential1: DecodedJwtCredential<Object> =
+    JwtCredentialValidator::with_signature_verifier(EdDSAJwsVerifier::default())
+    .validate::<_, Object>(
+        &jwt,
+        &issuer1_document,
+        &JwtCredentialValidationOptions::default(),
+        FailFast::FirstError,
+    )
+    .unwrap();
+
+    println!("VC successfully validated");
+
+    println!("Credential JSON > {:#}", decoded_credential1.credential);
 
   Ok(())
 }
